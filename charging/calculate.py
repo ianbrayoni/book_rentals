d788f135639456433b76ee1919d534ec6f628157
Brayoni Ian <brayoni.ian@gmail.com>
2020-08-17 17:09:34 +0300

    add charging module

---
+++ charging/calculate.py
@@ -0,0 +1,60 @@
+import uuid
+from datetime import datetime
+from django.conf import settings
+
+from api.models import Rentals, Invoices
+from utils.helpers import error_handler
+
+
+class Charges:
+    """
+    Calculate charges for books rented
+    """
+
+    DAILY_CHARGE = settings.DAILY_CHARGE
+
+    def __init__(self, rental=[]):
+        self.rental = rental
+
+    def _calculate_charge(self, quantity, duration):
+        return quantity * self.DAILY_CHARGE * duration
+
+    @error_handler
+    def calculate(self):
+        invoice_id = uuid.uuid4()
+        total_charges = 0
+        statement = dict(invoiceId=invoice_id, totalCharges=total_charges, charges=[])
+
+        # create invoice instance
+        invoice = Invoices(
+            id=invoice_id,
+            total_charges=total_charges,
+            date_generated=datetime.now().isoformat(),
+        )
+        invoice.save()
+
+        for data in self.rental:
+            charge = self._calculate_charge(data["quantity"], data["duration"])
+
+            charges_per_book = dict(bookId=data["book_id"], charge=charge)
+            statement["charges"].append(charges_per_book)
+
+            rental = Rentals(
+                book_id=data["book_id"],
+                invoice=invoice,
+                duration=data["duration"],
+                quantity=data["quantity"],
+                rental_date=data["rental_date"],
+                charge=charge,
+            )
+            rental.save()
+
+            total_charges += charge
+
+        # update total_charges in Invoice
+        invoice.total_charges = total_charges
+        invoice.save()
+
+        statement["totalCharges"] = total_charges
+
+        return statement
---
+++ charging/tests.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+
+from .calculate import Charges
+from api.models import Rentals, Invoices
+
+
+class CalculateTestCase(TestCase):
+    def setUp(self):
+        self.rental = [
+            {
+                "book_id": 1,
+                "quantity": 2,
+                "rental_date": "2020-08-15T12:12:59.785466",
+                "duration": 3,
+            },
+            {
+                "book_id": 2,
+                "quantity": 1,
+                "rental_date": "2020-08-15T12:12:59.785466",
+                "duration": 1,
+            },
+            {
+                "book_id": 3,
+                "quantity": 1,
+                "rental_date": "2020-08-15T12:12:59.785466",
+                "duration": 4,
+            },
+        ]
+        self.statement = Charges(rental=self.rental).calculate()
+
+    def test_invoice_and_rentals_records_created(self):
+        invoices = Invoices.objects.all()
+        rental_records = Rentals.objects.filter(invoice=invoices[0].id)
+        # one invoice is created for all 3 rentals
+        self.assertTrue(invoices.count() == 1)
+        self.assertTrue(rental_records.count() == 3)
+
+    def test_statement_created(self):
+        self.assertDictEqual(
+            self.statement,
+            {
+                "invoiceId": self.statement["invoiceId"],
+                "totalCharges": 11,
+                "charges": [
+                    {"bookId": 1, "charge": 6},
+                    {"bookId": 2, "charge": 1},
+                    {"bookId": 3, "charge": 4},
+                ],
+            },
+        )
